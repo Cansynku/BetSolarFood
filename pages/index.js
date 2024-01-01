01328883f3439329042c9142f6de70de2ffc3f8b
Javier Cano García <121675853+Cansynku@users.noreply.github.com>
2024-01-01 11:37:03 +0100

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -26,7 +26,7 @@ export default function HomePage() {
       </Head>
 
       <main>
-        <Header title="Welcome to my app!" />
+        <Header title="Welcome to my app JAVIER!" />
         <p className="description">
           Get started by editing <code>pages/index.js</code>
         </p>
